31666ab65ab599483e8f0333608bda82e68bb1b1
professorlara <126965783+professorlara@users.noreply.github.com>
2024-07-10 13:51:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,6 +83,10 @@ if st.button("Predict Emotion 🎤"):
         'num_trigrams', 'number_lines', 'num_bigrams',
         'num_unique_4grams', 'content_density'
         ]
+    train = pd.read_csv('TRAIN language data_1.csv',delimiter=";")
+    test = pd.read_csv('TEST language data.csv_1',delimiter=";")
+    validate= pd.read_csv('VAL language data.csv_1',delimiter=";")
+    
     
 
 
