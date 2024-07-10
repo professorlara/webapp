081cbe53183bce7b0182a99fe831b2753cdc4281
professorlara <126965783+professorlara@users.noreply.github.com>
2024-07-10 11:12:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,32 @@
 import streamlit as st
-st.write("Hello")
+
+st.markdown(
+    """
+    <style>
+    .big-font {
+        font-size:50px !important;
+    }
+    .medium-font {
+        font-size:30px !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
+st.markdown('<p class="big-font">🎵Music emotion prediction🎵</p>', unsafe_allow_html=True)
+
+st.markdown('<p class="medium-font">Please copy-paste the lyrics to your favourite song!😎</p>', unsafe_allow_html=True)
+
+
+lyrics = st.text_area("Lyrics input✏️", height=100)
+
+if st.button("Predict Emotion 🎤"):
+    status_placeholder = st.empty()
+    status_placeholder.write("Predicting emotion... 🔍")
+
+    status_placeholder.write("This may take some time... ⌛")
+
+    status_placeholder.empty()
+    st.write("Prediction completed! 🎉")
